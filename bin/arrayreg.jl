f33bb7cba786ae0952f1f9668e4eb328747a5992
Roger-luo <hiroger@qq.com>
2019-05-23 23:26:43 -0400

    add basic functionalities

---
+++ bin/arrayreg.jl
@@ -0,0 +1,15 @@
+using BenchmarkBot
+
+runbenchmarks(
+    package="YaoArrayRegister",
+    target=BenchmarkConfig(
+        id="origin/multithreading",
+        env = Dict("JULIA_NUM_THREADS"=>4),
+        juliacmd=`julia -O3`
+    )
+    baseline=BenchmarkConfig(
+        id="master",
+        env = Dict("JULIA_NUM_THREADS"=>1),
+        juliacmd=`julia -O3`
+    )
+)
--- src/BenchmarkBot.jl
+++ src/BenchmarkBot.jl
@@ -1,5 +1,50 @@
 module BenchmarkBot
 
-greet() = print("Hello World!")
+using PkgBenchmark, GitHub, JSON, UUIDs, Dates, Base64
+export BenchmarkConfig, submit_report, submit_issue, runbenchmarks
+
+@static if haskey(ENV, "GITHUB_BENCHMARK_TOKEN")
+const TOKEN = authenticate(ENV["GITHUB_BENCHMARK_TOKEN"])
+else
+const TOKEN = nothing
+end
+
+const REPO = repo("QuantumBFS/YaoBenchmarks.jl")
+
+function submit_report(repo, package, results)
+    file_json = JSON.parse(
+        """
+        {
+            "message": "automatic generated benchmark report",
+            "content": "$(base64encode(sprint(export_markdown, results)))"
+        }
+        """
+    )
+
+    t = Dates.now()
+    folder = join(["reports", year(t), month(t), day(t), package], "/")
+    filepath = join([folder, "/", string(uuid1()), ".md"])
+    create_file(repo, filepath, params=file_json, auth=TOKEN)
+    submit_issue(repo, filepath)
+end
+
+function submit_issue(repo, filepath)
+    issue_json = JSON.parse(
+        """
+        {
+            "title": "Benchmark Report Generated for multithreading",
+            "body": "benchmark report generated. See details at $(escape_string(string(file(repo, filepath).html_url)))",
+            "labels": ["benchmark report"]
+        }
+        """
+    )
+    
+    create_issue(repo; params=issue_json, auth=TOKEN)
+end
+
+function runbenchmarks(;package, target, baseline)
+    results = judge(package, target, baseline)
+    submit_report(REPO, package, results)
+end
 
 end # module
