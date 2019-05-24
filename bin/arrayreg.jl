0f313a038e9b3c3f73e5c93b378902f0dea91a2a
Roger-luo <hiroger@qq.com>
2019-05-23 23:28:11 -0400

    add another benchmark job

--- bin/arrayreg.jl
+++ bin/arrayreg.jl
@@ -13,3 +13,17 @@ runbenchmarks(
         juliacmd=`julia -O3`
     )
 )
+
+runbenchmarks(
+    package="YaoArrayRegister",
+    target=BenchmarkConfig(
+        id="origin/multithreading",
+        env = Dict("JULIA_NUM_THREADS"=>1),
+        juliacmd=`julia -O3`
+    )
+    baseline=BenchmarkConfig(
+        id="master",
+        env = Dict("JULIA_NUM_THREADS"=>1),
+        juliacmd=`julia -O3`
+    )
+)
