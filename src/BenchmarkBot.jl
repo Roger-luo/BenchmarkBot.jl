b17f56f81dc8e89382bcc9f76dacd74a88b8aef2
Roger-luo <hiroger@qq.com>
2019-05-24 22:18:25 -0400

    make token dynamic

--- src/BenchmarkBot.jl
+++ src/BenchmarkBot.jl
@@ -3,10 +3,12 @@ module BenchmarkBot
 using PkgBenchmark, GitHub, JSON, UUIDs, Dates, Base64
 export BenchmarkConfig, submit_report, submit_issue, runbenchmarks
 
-if haskey(ENV, "GITHUB_BENCHMARK_TOKEN")
-const TOKEN = authenticate(ENV["GITHUB_BENCHMARK_TOKEN"])
-else
-const TOKEN = nothing
+function token()
+    if haskey(ENV, "GITHUB_BENCHMARK_TOKEN")
+        return authenticate(ENV["GITHUB_BENCHMARK_TOKEN"])
+    else
+        error("set github token by export GITHUB_BENCHMARK_TOKEN")
+    end
 end
 
 const REPO = repo("QuantumBFS/YaoBenchmarks.jl")
@@ -24,7 +26,7 @@ function submit_report(repo, package, results)
     t = Dates.now()
     folder = join(["reports", year(t), month(t), day(t), package], "/")
     filepath = join([folder, "/", string(uuid1()), ".md"])
-    create_file(repo, filepath, params=file_json, auth=TOKEN)
+    create_file(repo, filepath, params=file_json, auth=token())
     submit_issue(repo, filepath)
 end
 
@@ -39,7 +41,7 @@ function submit_issue(repo, filepath)
         """
     )
     
-    create_issue(repo; params=issue_json, auth=TOKEN)
+    create_issue(repo; params=issue_json, auth=token())
 end
 
 function runbenchmarks(;package, target, baseline)
